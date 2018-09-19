6b5d69412dffc4968fed777786894574cdc9913d
Celso Marciano <cemarte83@gmail.com>
2018-09-19 18:21:41 -0400

    initial

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Shenanigans } from "react-microbundle-ts";
+const App: React.SFC = () => <div>
+    <h1>Test</h1>
+
+    <p>Component 1</p>
+    <Shenanigans />
+</div>;
+
+export default App;
